309dac0fdf9d44238adf5cae82cbc30f9380410a
Mark Steadman <marksteadman@me.com>
2014-04-05 15:16:39 +0100

    Renamed to avoid confusion with original

--- djhallo_grappelli/widgets.py
+++ djhallo_grappelli/widgets.py
@@ -7,25 +7,32 @@ from django.utils.html import escape
 from django.utils.encoding import force_unicode
 from django.utils.safestring import mark_safe
 
-
 class HalloInput(forms.Textarea):
-
     def render(self, name, value, attrs=None):
         value = value or ''
         final_attrs = self.build_attrs(attrs, name=name)
         html = [u'<div class="hallo-block"><article class="edit"></article><textarea%s>%s</textarea></div>' % \
-                (flatatt(final_attrs),
-                 force_unicode(escape(value)))]
+            (
+                flatatt(final_attrs),
+                force_unicode(escape(value))
+            )
+        ]
+
         return mark_safe(u'\n'.join(html))
 
     @property
     def media(self):
-        return forms.Media(css= {'screen': [settings.STATIC_URL + "djhallo/hallo.css"]},
-            js=(
+        return forms.Media(
+            css = {
+                'screen': ['%sdjhallo_grappelli/hallo.css' % settings.STATIC_URL]
+            },
+            js = (
                 '//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js',
                 '//ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js',
-                settings.STATIC_URL + "djhallo/jquery-init.js",
-                settings.STATIC_URL + "djhallo/showdown.js",
-                settings.STATIC_URL + "djhallo/to-markdown.js",
-                settings.STATIC_URL + "djhallo/hallo.js",
-                settings.STATIC_URL + "djhallo/djhallo.js"))
+                '%sdjhallo_grappelli/jquery-init.js' % settings.STATIC_URL,
+                '%sdjhallo_grappelli/showdown.js' % settings.STATIC_URL,
+                '%sdjhallo_grappelli/to-markdown.js' % settings.STATIC_URL,
+                '%sdjhallo_grappelli/hallo.js' % settings.STATIC_URL,
+                '%sdjhallo_grappelli/djhallo.js' % settings.STATIC_URL
+            )
+        )
